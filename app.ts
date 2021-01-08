f8eae3f03f68961c99b3b2466a62373d2df0ff7f
utek <lcpnine@gmail.com>
2021-01-09 01:37:22 +0900

    feat: add basic features
    - morgans: 'short' to check method, endpoint
    - cookieparset: to set cookie and check cookie later

--- app.ts
+++ app.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('short'));
 
 app.get('/', (req: Request, res: Response) =>
   res.send('Express + TypeScript Server')
