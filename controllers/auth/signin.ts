f7b0724dd9156fbf3b0cd594f4d93059e5cd3e23
utek <lcpnine@gmail.com>
2021-01-13 01:02:29 +0900

    refactor: seperate auth controller's signin to controller and model
    - use findUser in model/auth

--- controllers/auth/signin.ts
+++ controllers/auth/signin.ts
@@ -1,14 +1,10 @@
 import { Request, Response } from 'express';
-import prisma from '../../prisma/prisma';
+import findUser from '../../models/auth/findUser';
 
 const signIn = async (req: Request, res: Response): Promise<Response> => {
   const { email, password } = req.body;
   try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email,
-      },
-    });
+    const user = await findUser(email);
     if (!user) return res.status(400).send({ message: 'Invalid Email' });
     if (user.password !== password) {
       return res.status(400).send({ message: 'Invalid password' });
