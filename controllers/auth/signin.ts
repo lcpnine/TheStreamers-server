4bf8d7bd6846ad169acaf743aa0dae18f7179ec7
utek <lcpnine@gmail.com>
2021-01-13 03:09:40 +0900

    feat: update sign in to set cookie if success

--- controllers/auth/signin.ts
+++ controllers/auth/signin.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import findUser from '../../models/auth/findUser';
 import { comparePassword } from '../../utils/crypto';
+import { createJWT } from '../../utils/jwt';
 
 const signIn = async (req: Request, res: Response): Promise<Response> => {
   const { email, password } = req.body;
@@ -10,10 +11,12 @@ const signIn = async (req: Request, res: Response): Promise<Response> => {
     if (!user) return res.status(400).send({ message: 'Invalid Email' });
 
     const validPassword = await comparePassword(password, user.password);
-    if (validPassword) {
-      return res.status(200).send({ message: 'Signed in successfully' });
+    if (!validPassword) {
+      return res.status(400).send({ message: 'Invalid password' });
     }
-    return res.status(400).send({ message: 'Invalid password' });
+    const jwt = createJWT({ email: user.email, username: user.username });
+    res.cookie('jwt', jwt); // TODO: if find better way to deal with expiration, change options
+    return res.status(200).send({ message: 'Signed in successfully', jwt });
   } catch (err) {
     return res
       .status(500)
